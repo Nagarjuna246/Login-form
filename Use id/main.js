c6bf203f7e855281c2c6448306db4bad075ae317
Nagarjuna246 <59115578+Nagarjuna246@users.noreply.github.com>
2019-12-30 14:56:59 +0530

    login form useing (Name,Email) with HTML,CSS

---
+++ Use id/main.js
@@ -0,0 +1,10 @@
+function person(FirstName,LastName,DOB)
+{
+    this.FirstName=FirstName;
+    this.LastName=LastName;
+    this.DOB=DOB;
+}
+const person1=new person('nagarjuna','sanampudi','09-07-1997');
+console.log(person1);
+    
+
